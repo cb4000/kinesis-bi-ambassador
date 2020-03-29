8c7e99fada7e5770b9dce42ff601aa2823e81cda
cb4000 <mathfan1980@gmail.com>
2020-03-29 02:45:27 -0400

    Created a starting point with pring initializr

---
+++ kinesis-suite/src/main/java/io/datanomnom/kinesissuite/KinesisSuiteApplication.java
@@ -0,0 +1,13 @@
+package io.datanomnom.kinesissuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinesisSuiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinesisSuiteApplication.class, args);
+	}
+
+}
---
+++ kinesis-suite/src/test/java/io/datanomnom/kinesissuite/KinesisSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datanomnom.kinesissuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinesisSuiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
