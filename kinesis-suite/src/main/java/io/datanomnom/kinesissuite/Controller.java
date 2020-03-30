2037d91c2e3b0911dae408e64a49d5e5d0c2ba63
cb4000 <mathfan1980@gmail.com>
2020-03-30 13:23:41 -0400

    added some rudimentary functionality

--- kinesis-suite/src/main/java/io/datanomnom/kinesissuite/Controller.java
+++ kinesis-suite/src/main/java/io/datanomnom/kinesissuite/Controller.java
@@ -1,9 +1,15 @@
 package io.datanomnom.kinesissuite;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.amazonaws.services.kinesis.producer.KinesisProducerConfiguration;
+
+import java.nio.ByteBuffer;
+
+import com.amazonaws.services.kinesis.producer.KinesisProducer;
 @RestController
 public class Controller {
 
@@ -16,8 +22,29 @@ public class Controller {
   @PostMapping("/facts")
   String newFact(@RequestBody Fact newFact) {
     
-	System.out.println(newFact.toString());
+    System.out.println(newFact.toString());
+    factWriter(newFact.toString());
     return "got it"+newFact.toString();
   }
+void factWriter(String newFact){
+KinesisProducerConfiguration config = new KinesisProducerConfiguration()
+        .setRecordMaxBufferedTime(3000)
+        .setMaxConnections(1)
+        .setRequestTimeout(60000)
+        .setRegion("us-east-1");
+        
+final KinesisProducer kinesisProducer = new KinesisProducer(config);
+
+    // KinesisProducer gets credentials automatically like 
+// DefaultAWSCredentialsProviderChain. 
+// It also gets region automatically from the EC2 metadata service. 
+KinesisProducer kinesis = new KinesisProducer();  
+// Put some records 
+    ByteBuffer data = ByteBuffer.wrap("myData".getBytes());
+    // doesn't block       
+    kinesis.addUserRecord("fact-bi-poc", "myPartitionKey", data); 
+// Do other stuff ...
+}
+
 
 }
--- kinesis-suite/src/main/java/io/datanomnom/kinesissuite/Fact.java
+++ kinesis-suite/src/main/java/io/datanomnom/kinesissuite/Fact.java
@@ -6,10 +6,10 @@ public class Fact {
 	private final String when;
 	private final String what;
 
-	public Fact(String _source, String _when , String _what) {
-		this.source = _source;
-		this.when = _when;
-		this.what = _what;
+	public Fact(String source, String when , String what) {
+		this.source = source;
+		this.when = when;
+		this.what = what;
 	}
 
     public String toString(){
